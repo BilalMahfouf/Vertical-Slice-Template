c7e858b2f5c5431cbd97026fe9d1dde3e015b83a
unknown <billelmh501@gmail.com>
2026-01-25 09:17:31 +0100

    .

---
+++ backend/Veterinary/src/VeterinaryApi/Common/Extensions/MigrationExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VeterinaryApi.Infrastructure.Persistence;
+
+namespace VeterinaryApi.Common.Extensions;
+
+public static class MigrationExtension
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+        using ApplicationDbContext dbContext =
+            scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        dbContext.Database.Migrate();
+    }
+}
