2eeb4b5693cda239b1fbf2fc9266b5f06b96ea0b
BilalMahfouf <billelmh501@gmail.com>
2026-02-20 15:37:59 +0100

    modify cors config to allow frontend to access the api

--- backend/Veterinary/src/VeterinaryApi/Program.cs
+++ backend/Veterinary/src/VeterinaryApi/Program.cs
@@ -57,7 +57,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins("https://veterinary-app-nu.vercel.app/")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials()
