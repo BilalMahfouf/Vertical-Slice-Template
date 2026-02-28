8d33aa36f20abf4cc23fb2a9d8a92c703ed0cf2b
BilalMahfouf <billelmh501@gmail.com>
2026-02-28 08:35:28 +0100

    change db cn to production

--- backend/Veterinary/src/VeterinaryApi/Infrastructure/DependencyInjection.cs
+++ backend/Veterinary/src/VeterinaryApi/Infrastructure/DependencyInjection.cs
@@ -110,7 +110,7 @@ public static class DependencyInjection
 
         // ef core config  
         var connectionString = Environment
-            .GetEnvironmentVariable(AppSettings.DevConnectionStringName);
+            .GetEnvironmentVariable(AppSettings.ProductionConnectionStringName);
         services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(
             (sp, options) =>
         {
