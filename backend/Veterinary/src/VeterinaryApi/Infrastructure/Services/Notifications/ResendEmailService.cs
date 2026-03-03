ee02cb3e4bfec566b294b83e29e6b2034b313f04
BilalMahfouf <billelmh501@gmail.com>
2026-03-03 17:57:03 +0100

    replace gmail smpt with resend for email sending

---
+++ backend/Veterinary/src/VeterinaryApi/Infrastructure/Services/Notifications/ResendEmailService.cs
@@ -0,0 +1,38 @@
+﻿using Resend;
+using VeterinaryApi.Common.Abstracions.Emails;
+using VeterinaryApi.Common.Errors;
+using VeterinaryApi.Common.Results;
+
+namespace VeterinaryApi.Infrastructure.Services.Notifications;
+
+public sealed class ResendEmailService(
+    IResend resend,
+    ILogger<ResendEmailService>logger) : IEmailService
+{
+    public async Task<Result> SendEmailAsync(
+        SendEmailRequest request,
+        CancellationToken cancellationToken)
+    {
+
+        try
+        {
+            logger.LogInformation("sending email");
+            var message = new EmailMessage();
+            message.From = "billelgamer3@gmail.com";
+            message.Subject = request.Subject;
+            message.To = request.To;
+            message.HtmlBody = request.Body;
+
+            await resend.EmailSendAsync(message, cancellationToken);
+            logger.LogInformation("email send succsessfuly");
+            return Result.Success;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError($"Exception in the class {nameof(ResendEmailService)}" +
+                $" in the function {nameof(SendEmailAsync)}.\n" +
+                $"Ex: {ex}");
+            return Result.Failure(Error.Failure("", ""));
+        }
+    }
+}
