5f948ede09ca603890e97eb5065287d33c573f5c
BilalMahfouf <billelmh501@gmail.com>
2026-03-09 18:26:56 +0100

    fix cors (again)

--- backend/Veterinary/src/VeterinaryApi/Program.cs
+++ backend/Veterinary/src/VeterinaryApi/Program.cs
@@ -64,7 +64,7 @@ builder.Services.AddCors(options =>
               "http://localhost",
               "http://localhost:5173",
               "http://localhost:5174",
-              "https://www.aviavet.online/"
+              "https://www.aviavet.online"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
