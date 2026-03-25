fd880922f24e47701b9bf94d1d4af933f08937fd
BilalMahfouf <billelmh501@gmail.com>
2026-03-25 09:58:40 +0100

    change db connection string to production

--- backend/Veterinary/src/VeterinaryApi/Infrastructure/DependencyInjection.cs
+++ backend/Veterinary/src/VeterinaryApi/Infrastructure/DependencyInjection.cs
@@ -115,7 +115,7 @@ public static class DependencyInjection
         // ef core config  
 
         var connectionString = Environment
-            .GetEnvironmentVariable(AppSettings.DevConnectionStringName);
+            .GetEnvironmentVariable(AppSettings.ProductionConnectionStringName);
         services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(
             (sp, options) =>
         {
